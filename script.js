c54a83061da3003481a36216976647f08100c673
broyerdamien <broyerdamien@gmail.com>
2022-09-30 11:09:42 +0200

    homeVersion1

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+let button = document.querySelector('#mode');
+let span   = document.querySelector('span');
+
+if(localStorage.getItem('theme')){
+  if(localStorage.getItem('theme') == 'sombre') {
+    modeSombre();
+  }
+}
+
+button.addEventListener('click', () => {
+  if(document.body.classList.contains('dark')) {
+    document.body.className = '';
+    span.textContent = 'Thème sombre';
+    localStorage.setItem('theme', 'clair');
+  }
+  else {
+    modeSombre();
+  }
+});
+
+function modeSombre() {
+  document.body.className = 'dark';
+  span.textContent = 'Thème clair';
+  localStorage.setItem('theme', 'sombre');
+}
+
 
