addac1b87ed73e11b03dc38d97953c159e67700b
sébatien papet <papet.seb@gmail.com>
2022-09-30 15:05:45 +0200

    add js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const hamburgerBouton = document.querySelector(".nav-toggler")
+
+const navigation = document.querySelector("nav")
+
+hamburgerBouton.addEventListener("click", toggleNav)
+
+function toggleNav(){
+    hamburgerBouton.classList.toggle("active")
+    navigation.classList.toggle("active")
+}
 
