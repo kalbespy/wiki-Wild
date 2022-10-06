f07e44ee893ab1bdbd01923c953b25243a2a4915
Toussaint Charlie <charlie.toussaint@hotmail.fr>
2022-10-06 09:31:26 +0200

    Added the transition effect, cleaned the html and css

--- pageJS.php
+++ pageJS.php
@@ -120,14 +120,14 @@
 
 
             <article class="card wild article"> <a
-                    href='https://www.programiz.com/javascript/library/array#:~:text=In%20JavaScript%2C%20Array%20is%20a,default%20or%20custom%20sorting%20rules.'
+                    href='https://www.freecodecamp.org/news/the-complete-javascript-handbook-f26b2c71719c'
 
                     target="_blank">
 
                     <div class="souspageArticle js3">
 
                         <div class="categoryFilter">
-                            <p class="btnFilter"> Wild
+                            <p class="btnFilter"> Wilder
                             </p>
 
                             <p class="btnFilter"> Article
@@ -136,7 +136,7 @@
                     </div>
 
                     <div class="articleDescription">
-                        <h3>Liste de toutes les arrays javascript</h3>
+                        <h3>Le guide du débutant javascript</h3>
                         <p class="articleText">Toutes les arrays javascript avec leurs fonctions et des exemples.
                         </p>
                         <p class="date">29/09/22
@@ -149,9 +149,9 @@
 
             <!-- Article 4 -->
 
-            <article class="card wild rtfm"> <a href='https://developer.mozilla.org/fr/docs/Web/JavaScript' target="_blank">
+            <article class="card wild rtfm"> <a href='https://nouvelle-techno.fr/articles/manipuler-le-dom-en-javascript' target="_blank">
 
-                    <div class="souspageArticle js1">
+                    <div class="souspageArticle js4">
 
                         <div class="categoryFilter">
                             <p class="btnFilter"> Wild
@@ -163,7 +163,7 @@
                     </div>
 
                     <div class="articleDescription">
-                        <h3>Toute la doc Javascript Mozilla</h3>
+                        <h3>Liste de toutes les arrays javascript</h3>
                         <p class="articleText">Retrouvez toute la doc Javascript de Mozilla, une ressource sûr et
                             fiable.
                         </p>
@@ -177,22 +177,22 @@
 
             <!-- Article 5 -->
             
-            <article class="card wilder video"> <a href='https://www.youtube.com/watch?v=N8ap4k_1QEQ&ab_channel=ProgrammingwithMosh'
+            <article class="card wilder article"> <a href='https://nouvelle-techno.fr/articles/manipuler-le-dom-en-javascript'
                     target="_blank">
 
-                    <div class="souspageArticle js2">
+                    <div class="souspageArticle js5">
 
                         <div class="categoryFilter">
                             <p class="btnFilter"> Wilder
                             </p>
 
-                            <p class="btnFilter"> Vidéo
+                            <p class="btnFilter"> Article
                             </p>
                         </div>
                     </div>
 
                     <div class="articleDescription">
-                        <h3>Vidéo pour apprendre à gérer les fonctions</h3>
+                        <h3>Un résumé facile pour gérer les DOM</h3>
                         <p class="articleText">Apprenez à gerer les fonctions de base avec Mosh, un tutoriel facile à
                             suivre et à
                             comprendre.
@@ -210,23 +210,23 @@
 
 
 
-            <article class="card wild article"> <a
-                    href='https://www.programiz.com/javascript/library/array#:~:text=In%20JavaScript%2C%20Array%20is%20a,default%20or%20custom%20sorting%20rules.'
+            <article class="card wild video"> <a
+                    href='https://www.youtube.com/watch?v=gHD7FOONV2o&ab_channel=NouvelleTechno'
                     target="_blank">
 
-                    <div class="souspageArticle js3">
+                    <div class="souspageArticle js6">
 
                         <div class="categoryFilter">
-                            <p class="btnFilter"> Wild
+                            <p class="btnFilter"> Wilder
                             </p>
 
-                            <p class="btnFilter"> Article
+                            <p class="btnFilter"> Vidéo
                             </p>
                         </div>
                     </div>
 
                     <div class="articleDescription">
-                        <h3>Liste de toutes les arrays javascript</h3>
+                        <h3>Un tuto vidéo d'une heure pour manipuler le DOM</h3>
                         <p class="articleText">Toutes les arrays javascript avec leurs fonctions et des exemples.
                         </p>
                         <p class="date">29/09/22
@@ -239,9 +239,9 @@
 
             <!-- Article 7 -->
 
-            <article class="card wild rtfm"> <a href='https://developer.mozilla.org/fr/docs/Web/JavaScript' target="_blank">
+            <article class="card wild rtfm"> <a href='https://www.sitepoint.com/javascript-truthy-falsy/' target="_blank">
 
-                    <div class="souspageArticle js1">
+                    <div class="souspageArticle js7">
 
                         <div class="categoryFilter">
                             <p class="btnFilter"> Wild
@@ -253,7 +253,7 @@
                     </div>
 
                     <div class="articleDescription">
-                        <h3>Toute la doc Javascript Mozilla</h3>
+                        <h3>Comprendre les valeurs Truthy et Falsy</h3>
                         <p class="articleText">Retrouvez toute la doc Javascript de Mozilla, une ressource sûr et
                             fiable.
                         </p>
@@ -267,22 +267,22 @@
 
             <!-- Article 8 -->
 
-            <article class="card wilder video"> <a href='https://www.youtube.com/watch?v=N8ap4k_1QEQ&ab_channel=ProgrammingwithMosh'
+            <article class="card wilder article"> <a href='https://developer.mozilla.org/fr/docs/Web/JavaScript/Guide/Working_with_Objects'
                     target="_blank">
 
-                    <div class="souspageArticle js2">
+                    <div class="souspageArticle js8">
 
                         <div class="categoryFilter">
                             <p class="btnFilter"> Wilder
                             </p>
 
-                            <p class="btnFilter"> Vidéo
+                            <p class="btnFilter"> Article
                             </p>
                         </div>
                     </div>
 
                     <div class="articleDescription">
-                        <h3>Vidéo pour apprendre à gérer les fonctions</h3>
+                        <h3>Utiliser les objets avec Mozilla</h3>
                         <p class="articleText">Apprenez à gerer les fonctions de base avec Mosh, un tutoriel facile à
                             suivre et à
                             comprendre.
@@ -298,15 +298,75 @@
 
             <!-- Article 9 -->
 
-            <article class="card wild article"> <a
-                    href='https://www.programiz.com/javascript/library/array#:~:text=In%20JavaScript%2C%20Array%20is%20a,default%20or%20custom%20sorting%20rules.'
+            <article class="card wild jeux"> <a
+                    href='https://www.codewars.com/join?language=javascript'
 
                     target="_blank">
 
-                    <div class="souspageArticle js3">
+                    <div class="souspageArticle js9">
 
                         <div class="categoryFilter">
                             <p class="btnFilter"> Wild
+                            </p>
+
+                            <p class="btnFilter"> Jeu
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="articleDescription">
+                        <h3>CodeWars JS - L'entrainement shonen commence</h3>
+                        <p class="articleText">Toutes les arrays javascript avec leurs fonctions et des exemples.
+                        </p>
+                        <p class="date">29/09/22
+                        </p>
+                    </div>
+                </a>
+                
+
+            </article>
+
+
+                        <!-- Article 10 -->
+
+                        <article class="card wilder video"> <a
+                    href='https://www.youtube.com/watch?v=jZZDfl6Jq2o&list=PLjwdMgw5TTLVjLr12-t9jIa1nX8ViUYjV&ab_channel=Grafikart.fr'
+
+                    target="_blank">
+
+                    <div class="souspageArticle js10">
+
+                        <div class="categoryFilter">
+                            <p class="btnFilter"> Wilder
+                            </p>
+
+                            <p class="btnFilter"> Vidéo
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="articleDescription">
+                        <h3>Apprenez à créer un puissance 4</h3>
+                        <p class="articleText">Toutes les arrays javascript avec leurs fonctions et des exemples.
+                        </p>
+                        <p class="date">29/09/22
+                        </p>
+                    </div>
+                </a>
+
+                </article>
+
+                            <!-- Article 11 -->
+
+            <article class="card wilder article"> <a
+                    href='https://freefrontend.com/html-menu-with-javascript/'
+
+                    target="_blank">
+
+                    <div class="souspageArticle js11">
+
+                        <div class="categoryFilter">
+                            <p class="btnFilter"> Wilder
                             </p>
 
                             <p class="btnFilter"> Article
@@ -315,7 +375,7 @@
                     </div>
 
                     <div class="articleDescription">
-                        <h3>Liste de toutes les arrays javascript</h3>
+                        <h3>Collection de menu de navigation JavaScript natif gratuit</h3>
                         <p class="articleText">Toutes les arrays javascript avec leurs fonctions et des exemples.
                         </p>
                         <p class="date">29/09/22
