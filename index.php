11682830a44c2cb9104b4710fb8e2b1a7b3dbeb0
Toussaint Charlie <charlie.toussaint@hotmail.fr>
2022-10-07 10:40:31 +0200

    FInal 1.1

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
                 <h3>	Brendan Eich / 1996</h3>
                 <p>JavaScript est un langage de programmation de scripts principalement employé dans les pages web interactives et à ce titre 
                     est une partie essentielle des applications web. Avec les langages HTML et CSS, JavaScript est au cœur des langages utilisés 
-                    par les développeurs web. Une grande majorité des sites web l'utilisent4, et la majorité des navigateurs web disposent d'un
+                    par les développeurs web. Une grande majorité des sites web l'utilisent, et la majorité des navigateurs web disposent d'un
                      moteur JavaScript pour l'interpréter.</p>
                 <a href="pageJS.php" ><button type="button" class="buttonHome">Voir toutes les docs</button></a>
             </div>
